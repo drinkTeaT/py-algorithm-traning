0def21e82f9d1313f499afa7c42b0cac30815dfb
tacbin <oh.ulaula@gmail.com>
2020-12-20 12:51:40 +0800

    优化：新增总结

--- 20201220-continue/1 常用数据结构和算法.py
+++ 20201220-continue/1 常用数据结构和算法.py
@@ -1,5 +1,8 @@
+from common.common_class import ListNode
+
+
 # 242. 有效的字母异位词 https://leetcode-cn.com/problems/valid-anagram/
-class Solution:
+class ValidAnagram:
     def is_anagram(self, s: str, t: str) -> bool:
         s_map = {}
         for i in s:
@@ -18,3 +21,20 @@ class Solution:
                 else:
                     s_map[i] = count - 1
         return len(s_map) == 0
+
+
+# 剑指 Offer 24. 反转链表 https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+class FanZhuanLianBiao:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        prev = None
+        p1 = head
+        p2 = p1.next
+        while p2 is not None:
+            p1.next = prev
+            prev = p1
+            p1 = p2
+            p2 = p2.next
+        p1.next = prev
+        return p1
