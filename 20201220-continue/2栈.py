92ec11b7edb39f45768bb45012fa18aa2ed849e9
liuweibin <liuweibin3@sfmail.sf-express.com>
2020-12-22 19:57:35 +0800

    新增：新增每日温度题解

--- 20201220-continue/2栈.py
+++ 20201220-continue/2栈.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 # 20. 有效的括号 https://leetcode-cn.com/problems/valid-parentheses/
 class ValidParentheses:
     def isValid(self, s: str) -> bool:
@@ -20,4 +23,25 @@ class ValidParentheses:
                     return False
         return len(my_stack) == 0
 
+
 # 739. 每日温度 https://leetcode-cn.com/problems/daily-temperatures/
+class DailyTemperatures:
+    def daily_temperatures(self, temps: List[int]) -> List[int]:
+        if temps is None:
+            return None
+        length = len(temps)
+        res = list(i * 0 for i in temps)
+        my_stack = list()
+        count = 0
+        while count < length:
+            if len(my_stack) == 0:
+                my_stack.append(count)
+            elif temps[my_stack[-1]] >= temps[count]:
+                my_stack.append(count)
+            else:
+                while len(my_stack) > 0 and temps[count] > temps[my_stack[-1]]:
+                    point = my_stack.pop(-1)
+                    res[point] = count - point
+                my_stack.append(count)
+            count += 1
+        return res
