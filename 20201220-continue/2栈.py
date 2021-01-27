29cb46b59455096f5e118491ce2f2ccfe9aef44a
liuweibin <liuweibin3@sfmail.sf-express.com>
2021-01-27 11:35:29 +0800

    新增栈的总结

--- 20201220-continue/2栈.py
+++ 20201220-continue/2栈.py
@@ -25,23 +25,26 @@ class ValidParentheses:
 
 
 # 739. 每日温度 https://leetcode-cn.com/problems/daily-temperatures/
+# 请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用0 来代替。
+# 例如，给定一个列表temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是[1, 1, 4, 2, 1, 1, 0, 0]。
+# 提示：气温 列表长度的范围是[1, 30000]。每个气温的值的均为华氏度，都是在[30, 100]范围内的整数。
+# 向右遍历，如果遇到大于目前值则计算天数，小于或等于则记录至queue
 class DailyTemperatures:
     def daily_temperatures(self, temps: List[int]) -> List[int]:
         if temps is None:
-            return None
-        length = len(temps)
-        res = list(i * 0 for i in temps)
-        my_stack = list()
-        count = 0
-        while count < length:
-            if len(my_stack) == 0:
-                my_stack.append(count)
-            elif temps[my_stack[-1]] >= temps[count]:
-                my_stack.append(count)
-            else:
-                while len(my_stack) > 0 and temps[count] > temps[my_stack[-1]]:
-                    point = my_stack.pop(-1)
-                    res[point] = count - point
-                my_stack.append(count)
-            count += 1
+            return temps
+        res = [0 for i in range(len(temps))]
+        queue = []
+        for i in range(len(temps)):
+            while len(queue) != 0 and temps[i] > temps[queue[-1]]:
+                res[queue.pop()] = i - queue[-1]
+            queue.append(i)
         return res
+
+
+# 84. 柱状图中最大的矩形 https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
+# 输入: [2,1,5,6,2,3]
+# 输出: 10
+class LargestRectangle:
+    def solve(self, heights: list[int]) -> int:
+        return 0
--- 20201220-continue/plan_test.py
+++ 20201220-continue/plan_test.py
@@ -1,37 +1,11 @@
-from common.common_class import ListNode, Generate, PrintUtil
-
-root = ListNode(0)
-root1 = ListNode(1)
-root2 = ListNode(2)
-root3 = ListNode(3)
-root4 = ListNode(4)
-root5 = ListNode(5)
-root6 = ListNode(6)
-root7 = ListNode(7)
-
-root.next = root1
-root1.next = root2
-root2.next = root3
-root3.next = root4
-root4.next = root5
-root5.next = root6
-root6.next = root7
+# 输入: [2,1,5,6,2,3]
+# 输出: 10
+# 向右遍历，记录最左边的坐标。遇到大于的： 遇到小于的：记录面积最大值，继续向右 遇到等于的：继续向右
+from typing import List
 
 
-def do_reverse(last: ListNode, start: ListNode, end: ListNode):
-    prev = None
-    curr = start
-    next_one = start.next
-    p1 = end.next
-    while next_one != end.next:
-        curr.next = prev
-        prev = curr
-        curr = next_one
-        next_one = next_one.next
-    curr.next = prev
-    last.next = curr
-    start.next = p1
+def solve(heights: list[int]) -> int:
+    return 0
 
 
-do_reverse(root1, root2, root5)
-PrintUtil.print_nodes(root)
+
